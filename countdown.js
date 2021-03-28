748358a0947d4d5b6fab8768aac6db9f9016e5ba
L3 Gang <65771028+L3gang@users.noreply.github.com>
2021-03-28 16:20:24 +0800

    Update countdown.js

--- countdown.js
+++ countdown.js
@@ -1,8 +1,8 @@
 // Set the date we're counting down to
-var countDownDate = new Date("May 22, 2021 00:00:01").getTime();
+var countDownDate = new Date("May 21, 2021 20:00:01").getTime();
 
 // Update the count down every 1 second
-var x = setInterval(function() {
+var x = setInterval(async function() {
 
   // Get today's date and time
   var now = new Date().getTime();
@@ -23,6 +23,49 @@ var x = setInterval(function() {
   // If the count down is finished, write some text
   if (distance < 0) {
     clearInterval(x);
-    document.getElementById("countdown").innerHTML = "EXPIRED";
+	
+	var hamster = document.getElementById("hamster");
+	
+	console.log("Hamster end.");
+	
+    document.getElementById("countdown").innerHTML = "Goodbye...";
+    document.getElementById("thanks").innerHTML = "Thank you for being part of the gang.";
+	
+	hamster.src = 'assets/l3pew.gif';
+	
+	await sleep(3000);
+	
+	hamster.src = 'assets/l3.png';
+	
+	await sleep(2000);
+	
+	fadeOut(hamster);
+	
+	hamster.style.opacity = 1;
+	
+	await sleep(10000);
+	
+	window.location.replace("https://www.youtube.com/watch?v=p3G5IXn0K7A");
   }
 }, 1000);
+
+function fadeOut(el) {
+	return new Promise(function (resolve, reject) {
+		let opacity = 1;
+		function fade(){
+		if ((opacity -= .01) > 0){
+				el.style.opacity = opacity;
+				requestAnimationFrame(fade);
+			} else {
+				resolve();
+				document.getElementById("hamster").src = 'assets/ctw.png';
+				el.style.opacity = 1;
+			}
+		}
+		fade();
+	});
+}
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
